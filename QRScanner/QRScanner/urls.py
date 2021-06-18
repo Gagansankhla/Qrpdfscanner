060f02521e7a23c80872ec01d731caa0153c73b7
Kushal Gandhi <kushalgandhi2601@gmail.com>
2021-06-18 12:05:00 +0530

    generate QR code

--- QRScanner/QRScanner/urls.py
+++ QRScanner/QRScanner/urls.py
@@ -23,7 +23,7 @@ from . import views
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-
-    path('page1', views.page1, name='webpage1')
+    path('page1', views.page1, name='webpage1'),
+    path('mobilePage', views.mobilePage, name='mobileWebPage')
 
 ]
--- QRScanner/QRScanner/views.py
+++ QRScanner/QRScanner/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import qrcode
+
+img = qrcode.make("http://127.0.0.1:8000/mobilePage")
+img.save("D:\QR Scanner\QRScanner\static\Images\qrcode.jpg")
 
 
 def index(request):
     return render(request, 'index.html')
 
+
 def page1(request):
     return render(request, 'page1.html')
 
+
+def mobilePage(request):
+    return render(request, 'mobilepage.html')
